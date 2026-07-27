aca0858593c60ceef709b451dfcdab385e4b64c1
danil <danilopupload@gmail.com>
2026-07-28 00:43:29 +0200

    stuck fader to 0db

--- src/main/java/com/bitwig/extensions/controllers/novation/launchcontrolxlmk3/bindings/SliderBinding.java
+++ src/main/java/com/bitwig/extensions/controllers/novation/launchcontrolxlmk3/bindings/SliderBinding.java
@@ -5,19 +5,18 @@ import com.bitwig.extension.controller.api.AbsoluteHardwareControlBinding;
 import com.bitwig.extension.controller.api.Parameter;
 
 public class SliderBinding extends LauncherBinding<AbsoluteHardwareControl> {
-    
+
     public SliderBinding(final ControlTargetId targetId, final Parameter parameter,
         final AbsoluteHardwareControl control) {
         super(targetId, control, parameter); //index + 0x05
     }
-    
+
     protected AbsoluteHardwareControlBinding getHardwareBinding() {
-        return getTarget().addBinding(getSource());
+        return getTarget().addBindingWithRange(getSource(), 0.0, 0.7953);
     }
-    
     @Override
     protected void updateValue() {
         // nothing to do
     }
-    
+
 }
